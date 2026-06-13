4737b90dae055a92c7cf49a5e276fe2d69548200
Nick Tem <nola234@icloud.com>
2026-06-13 14:50:44 +0200

    feat(db): add main function

--- db/db.py
+++ db/db.py
@@ -6,16 +6,12 @@ def get_connection():
     
     conn = sqlite3.connect('db/movies.db')
     conn.execute('PRAGMA foreign_keys = ON')
-    
-    print('Connection established to SQLite database.')
-    
+        
     return conn
 
 def create_tables(conn):
     cursor = conn.cursor()
-    
-    print('Creating tables if they do not exist...')
-    
+        
     conn.execute('''
         create table if not exists movie (
             movie_id integer primary key,
@@ -113,20 +109,15 @@ def create_tables(conn):
     
     conn.commit()
     
-    print('Tables created successfully.')
     cursor.close()
 
 def insert_data(conn):
     df = pd.read_csv('data/movies.csv')
-    
-    print(f'✓ Loaded CSV with: {df.shape[0]} rows and {df.shape[1]} columns.')
-    
+        
     movie_columns = ["title", "release_year", "duration", "budget", "box_office"]
     df_movie = df[movie_columns].drop_duplicates(subset=["title", "release_year"])
     df_movie.to_sql('movie', conn, if_exists='append', index=False)
-    
-    print(f'Inserted {len(df_movie)} unique movies into movie table.')
-    
+        
     for i, row in df.iterrows():
         movie_id = conn.execute('select movie_id from movie where title = ? and release_year = ?', (row['title'], row['release_year'])).fetchone()[0]
         
@@ -157,8 +148,13 @@ def insert_data(conn):
                 conn.execute('INSERT OR IGNORE INTO director (name) VALUES (?)', (director,))
                 director_id = conn.execute('SELECT director_id FROM director WHERE name = ?', (director,)).fetchone()[0]
                 conn.execute('INSERT OR IGNORE INTO movie_directors VALUES (?, ?)', (director_id, movie_id))
-    
-    print(f'Inserted data for {len(df)} movies into language, country, genre, and director tables.')
-    
+        
     conn.commit()
 
+def main():
+    conn = get_connection()
+    create_tables(conn)
+    insert_data(conn)
+    conn.close()
+    
+main()
