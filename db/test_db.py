883bc67022e532ea6f8312dd7475f557a07eb6ba
Nick Tem <nola234@icloud.com>
2026-07-04 14:02:26 +0200

    refactor(test_db): replace db.main() with direct create_tables and insert_data calls for safe setup

--- db/test_db.py
+++ db/test_db.py
@@ -1,32 +1,44 @@
-from db import get_connection
+import db
 
 def test():
     try:
-        conn = get_connection()
-        cursor = conn.cursor()
+        conn = db.get_connection()
+        test_query = "SELECT name FROM sqlite_master WHERE type='table' AND name='movie';"
+        print(f'Testing database connection and existence of tables...')
         
-        # Verify data
-        print(f"\nDatabase stats:")
-        
-        cursor.execute('SELECT COUNT(*) FROM movie')
-        print(f"Movies: {cursor.fetchone()[0]}")
-        
-        cursor.execute('SELECT COUNT(*) FROM language')
-        print(f"Languages: {cursor.fetchone()[0]}")
-        
-        cursor.execute('SELECT COUNT(*) FROM country')
-        print(f"Countries: {cursor.fetchone()[0]}")
-        
-        cursor.execute('SELECT COUNT(*) FROM genre')
-        print(f"Genres: {cursor.fetchone()[0]}")
-        
-        cursor.execute('SELECT COUNT(*) FROM director')
-        print(f"Directors: {cursor.fetchone()[0]}")
+        if conn.execute(test_query).fetchone() is None:
+            print('Database does not exist. Creating database and inserting data...')
+            db.create_tables(conn)
+            db.insert_data(conn)
+        else:
+            print('Database already exists.')
+            cursor = conn.cursor()
+            
+            # Verify data
+            print(f"\nDatabase stats:")
+            
+            cursor.execute('SELECT COUNT(*) FROM movie')
+            print(f"Movies: {cursor.fetchone()[0]}")
+            
+            cursor.execute('SELECT COUNT(*) FROM language')
+            print(f"Languages: {cursor.fetchone()[0]}")
+            
+            cursor.execute('SELECT COUNT(*) FROM country')
+            print(f"Countries: {cursor.fetchone()[0]}")
+            
+            cursor.execute('SELECT COUNT(*) FROM genre')
+            print(f"Genres: {cursor.fetchone()[0]}")
+            
+            cursor.execute('SELECT COUNT(*) FROM director')
+            print(f"Directors: {cursor.fetchone()[0]}")
         
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Error connecting to the database: {e}')
     
     finally:
-        conn.close()
+        try:
+            conn.close()
+        except Exception as e:
+            print(f'Connection unavailable: {e}')
 
 test()
